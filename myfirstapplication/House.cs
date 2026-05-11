a6abf68b7aa19db939d28e7a9b2d4cb46ec47fc1
jeremy <ytclace@gmail.com>
2026-05-11 12:21:56 -0500

    CAC-103 c# overview

---
+++ myfirstapplication/House.cs
@@ -0,0 +1,17 @@
+﻿
+namespace myfirstapplication;
+
+internal class House
+    {
+    string foundation;
+        string roofType;
+    string doorColor;
+    int windowSize;
+
+    public void DoorOpenClose()
+    {
+        Console.WriteLine("My door is open.")
+    }
+
+    }
+

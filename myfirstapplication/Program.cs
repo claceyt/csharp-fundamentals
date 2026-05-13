c18131de5a320615d5556720407e754465c630d7
jeremy <ytclace@gmail.com>
2026-05-13 16:07:22 -0500

    CAC-103 Lesson1 Value Types

--- myfirstapplication/Program.cs
+++ myfirstapplication/Program.cs
@@ -1,11 +1,11 @@
 ﻿using myfirstapplication;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 // The below statement calls a local function
 //single line comment
-HouseExample();
+
 
 /*
  Multi-line comment
@@ -14,14 +14,19 @@ are inside of members and are private by default. This means
 they are specific to that member and can only be called
 inside that member.
  */
-void HouseExample()
+
+// Lesson Examples
+//HouseExample();
+Lesson1Example()
+
+void HouseExample();
 {
-    House myHouse = new House();
-    myHouse.DoorOpenClose();
+   House myHouse = new House();
+   myHouse.DoorOpenClose();
 }
 void MyLocalFunction()
 {
-    This Local Function is for my first exercise lab.
+    // This Local Function is for my first exercise lab.
 }
 {
     Console.WriteLine(This is my first sentence);
@@ -29,6 +34,18 @@ Console.WriteLine(I am new to CSharp);
 Console.WriteLine(This CSharp course is cool);
 Console.WriteLine(I am learning stuff every day);
     }
+
+// Lesson 1 Value Types
+void Lesson1Example()
+{
+    Lesson1 myLesson = new Lesson1();
+    myLesson.intMinMax();
+    myLesson.IntegralExample();
+    myLesson.ConvertFloatToInt();
+    myLesson.LongFromInt();
+}
+
+
 
 
 

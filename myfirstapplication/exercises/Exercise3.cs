56b1154762d5d1146cbbd3fb3c00954a303bd51f
jeremy <ytclace@gmail.com>
2026-05-21 12:13:23 -0500

    Exercise3.cs

---
+++ myfirstapplication/exercises/Exercise3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myfirstapplication.exercises
+{
+    internal class Exercise3
+    {
+        public int CalculateModulus(int divisor)
+        {
+            int a = 15;
+            int b = 456;
+            int c = 23;
+            int d = 89;
+            int e = 245;
+            Console.WriteLine(a % divisor);
+            Console.WriteLine(b % divisor);
+                Console.WriteLine(c % divisor);
+            Console.WriteLine(d % divisor);
+            return e % divisor;
+
+        }
+
+        public void SolveMathproblems()
+        {
+            // a output 138
+            Console.WriteLine(10 + 32 * 12 / 3);
+            // b output 168
+            Console.WriteLine(10 + 32 * 5 - 2);
+            // c output 131
+            Console.WriteLine(10 + 121);
+        }
+    }
+
+    public void Calculate(short first, short second)
+        {
+            short result = first;
+            result += second;
+            Console.WriteLine($'+= result: {result}');
+            result = first;
+            result /= second;
+            Console.WriteLine($"/= result: {result}");
+            result = first;
+            result *= second;
+            Console.WriteLine($*= result: { result});
+            result = first;
+            result %= second;
+            Console.WriteLine($"%= result: {result}");
+        }
+
+        public void BooleanLogic(bool arg1, bool arg2)
+        {
+            Console.WriteLine(arg2 & arg1);
+            Console.WriteLine(arg2 | arg1);
+            Console.WriteLine(arg2 ^ arg1);
+            Console.WriteLine(arg2 || arg1);
+        }
+}

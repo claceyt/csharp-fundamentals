959fa1941fdaee77a8c799e14011e161ec74e85c
jeremy <ytclace@gmail.com>
2026-05-21 19:36:53 -0500

    Exercise4.cs

---
+++ myfirstapplication/exercises/Exercise4.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace myfirstapplication;
+
+internal class Exercise4
+{
+public void Strings(string first, string second)
+    {
+        if(first == second)
+        {
+            Console.WriteLine("The strings are equal.");
+        }
+        else
+        {
+            Console.WriteLine("The strings are not equal");
+        }
+    }
+
+    public string Grades(char grade)
+    {
+        return grade switch
+        {
+            'e' => "excellent",
+            'v' => "very good",
+            'g' => "good",
+            'a' => "average",
+            'f' => "fail",
+            = "not a valid grade"
+        };
+    }
+
+    public void displayThree()
+    {
+        for (int i = 0; i <= 30; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
+
+
+
+
+
+
+}
+
+

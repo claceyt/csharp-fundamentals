946cae848095790270c64b8ac6ff4fbdea85d262
jeremy <ytclace@gmail.com>
2026-05-29 17:57:52 -0500

    Boat and Horse.cs

---
+++ myfirstapplication/exercises/Boat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myfirstapplication.exercises
+{
+    internal class Boat
+    {
+        public Boat(string boatSize, string boatMaterial, string boatColor)
+        {
+          boatSize = boatSize ;
+           boatMaterial = boatMaterial;
+            boatColor = boatColor;
+        }
+
+        public Boat(string boatSize, string boatColor)
+            : this("Across", boatSize, boatColor)
+        { }
+
+
+        public Boat()
+                : this("Water", "wood")
+        { }
+    }
+}
---
+++ myfirstapplication/exercises/Horse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace myfirstapplication.exercises
+{
+    internal class Horse
+    {
+        public Horse(string tailType, string horseShape, string horseColor)
+        {
+            tailType = tailType;
+            horseShape = horseShape;
+            horseColor = horseColor;
+        }
+
+        public Horse(string horseShape, string horseColor)
+            : this("Flowing", horseShape, horseColor)
+        { }
+    
+
+    public Horse()
+            : this("Muscular", "Chestnut")
+        { }
+    }
+}

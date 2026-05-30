460406ef383de98769db70f9bbd2d319a0948d19
jeremy <ytclace@gmail.com>
2026-05-29 20:20:15 -0500

    Boat and Horse Excersise.cs

---
+++ myfirstapplication/exercises/ChildBoat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myfirstapplication;
+public class Boat
+{
+    public string Accrosswater { get; set; }
+    public string BoatSize{ get; set; }
+    public string BoatColor{ get; set; }
+    public string BoatMaterial{ get; set; }
+
+
+    public Boat() { }
+
+    public Boat(string Accrosswater, string BoatSize, string BoatColor, string BoatMaterial)
+    {
+        Accrosswater = Accrosswater;
+        BoatSize = BoatSize;
+        BoatColor = BoatColor;
+        BoatMaterial = BoatMaterial;
+    }
+}
+
+public class SailBoat : Boat
+{
+    public string Row { get; set; }
+
+
+    public SailBoat(string Accrosswater, string BoatSize, string BoatColor, string BoatMaterial, string Row)
+        : base(Accrosswater, BoatSize, "Unknown Color", Row)
+
+    {
+        Row = Row;
+    }
+      
+
+
+    public SailBoat(string Accrosswater, string BoatSize, string Row)
+         : this(Accrosswater, BoatSize, "Stocky", "Black", Row)
+    { }
+
+}
+
+
---
+++ myfirstapplication/exercises/ChildHorse.cs
@@ -0,0 +1,45 @@
+﻿
+
+using Myfirstapplication;
+
+namespace Myfirstapplication;
+
+public class Horse
+{
+    public string HorseRunning { get; set; }
+    public string TypeOfTail { get; set; }
+    public string HorseShape { get; set; }
+    public string HorseColor { get; set; }
+
+
+    public Horse() { }
+
+    public Horse(string horseRunning, string tailType, string horseShape, string horseColor)
+    {
+        HorseRunning = horseRunning;
+        TypeOfTail = tailType;
+        HorseShape = horseShape;
+        HorseColor = horseColor;
+    }
+}
+
+public class Stallion : Horse
+{
+    public string Clydesdale { get; set; }
+
+
+    public Stallion(string horseRunning, string typeOfTail, string horseShape, string horseColor, string clydesdale)
+        : base(horseRunning, typeOfTail, "Unknown Color", clydesdale)
+
+    {
+        Clydesdale = clydesdale;
+    }
+
+
+    public Stallion(string horseRunning, string typeOfTail, string clydesdale)
+         : this(horseRunning, typeOfTail, "Stocky", "Black", clydesdale)
+    { }
+
+}
+
+

de741bd71b1e0f3a53e9d8e4ece82828154fe184
jeremy <ytclace@gmail.com>
2026-05-31 12:47:39 -0500

    Exercise14.cs

---
+++ myfirstapplication/exercises/Exercise14.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Myfirstapplication;
+
+internal class Exercise14
+{
+    static void Main()
+    {
+        DisplayIceCreamFlavors();
+    }
+    static void DisplayIceCreamFlavors()
+    {
+        string[] flavors = { "vanilla", "chocolate", "strawberry", "mint chocolate chip", "coockie dough" };
+        foreach (string flavor in flavors)
+        {
+        Console.WriteLine(flavor);
+        }
+    }
+
+    public static void PrintChiefsScores()
+    {
+        
+        int[,] chiefsScores =
+        {
+            { 23, 34, 43 },
+            { 34, 35, 22 }
+        };
+
+        Console.WriteLine("Chiefs Scores:");
+
+      
+        foreach (int score in chiefsScores)
+        {
+            Console.Write(score + " ");
+        }
+
+        Console.WriteLine(); 
+    }
+
+    static void Scores()
+    {
+        PrintChiefsScores();
+    }
+
+}
